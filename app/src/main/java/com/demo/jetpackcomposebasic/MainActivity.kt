43452d10bcb816846b513d086220fa10a54c622f
Jorge Agullo <agullojorge@gmail.com>
2022-06-02 17:41:35 +0200

    End

--- app/src/main/java/com/demo/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/demo/jetpackcomposebasic/MainActivity.kt
@@ -11,10 +11,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.demo.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +43,67 @@ fun Figure(color: Color) {
 
 @Composable
 fun ConstraintsComposable() {
+    val constraint = ConstraintSet() {
+        val greenBox = createRefFor("greenBox")
+        val redBox = createRefFor("redBox")
+        val blueBox = createRefFor("blueBox")
+        val yellowBox = createRefFor("yellowBox")
+
+        constrain(greenBox) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+
+        constrain(redBox) {
+            top.linkTo(greenBox.top, margin = 50.dp)
+            start.linkTo(greenBox.start, margin = 50.dp)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+
+        constrain(blueBox) {
+            top.linkTo(redBox.bottom)
+            start.linkTo(redBox.end)
+            width = Dimension.value(200.dp)
+            height = Dimension.value(200.dp)
+        }
+
+        constrain(yellowBox) {
+            top.linkTo(blueBox.top, margin = 50.dp)
+            end.linkTo(blueBox.end, margin = 50.dp)
+            width = Dimension.value(250.dp)
+            height = Dimension.value(250.dp)
+        }
+
+
+    }
+
+    ConstraintLayout(constraint, modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .layoutId("greenBox")
+        )
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .layoutId("redBox")
+        )
+        Box(
+            modifier = Modifier
+                .background(Color.Blue)
+                .layoutId("blueBox")
+        )
+        Box(
+            modifier = Modifier
+                .background(Color.Yellow)
+                .layoutId("yellowBox")
+        )
+
+    }
+
 
 }
 
