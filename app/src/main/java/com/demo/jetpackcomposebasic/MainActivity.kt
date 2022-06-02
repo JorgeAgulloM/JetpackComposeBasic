9c75b2ec8fce4bba313f01cb5b71882490affc2f
Jorge Agullo <agullojorge@gmail.com>
2022-06-02 17:58:23 +0200

    finish

--- app/src/main/java/com/demo/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/demo/jetpackcomposebasic/MainActivity.kt
@@ -3,9 +3,18 @@ package com.demo.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.demo.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +28,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun StatesComposableComposable() {
+    var numRemember by remember { mutableStateOf(0) }
+    var numRememberSaveable by rememberSaveable { mutableStateOf(0) }
+    Column(modifier = Modifier.fillMaxSize()) {
 
+        Text(text = "conteo: $numRemember")
+
+        Divider(thickness = 20.dp, color = Color.Transparent)
+
+        Button(onClick = { numRemember++ }) {
+            Text(text = "Sumar")
+        }
+
+        Divider()
+
+        Text(text = "conteo: $numRememberSaveable")
+
+        Divider(thickness = 20.dp, color = Color.Transparent)
+
+        Button(onClick = { numRememberSaveable++ }) {
+            Text(text = "Sumar")
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true, device = Devices.DEFAULT)
