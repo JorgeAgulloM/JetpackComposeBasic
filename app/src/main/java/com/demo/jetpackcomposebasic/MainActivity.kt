93911ddb4028d63d917956828d4a84ede9d3b876
Jorge Agullo <agullojorge@gmail.com>
2022-06-03 08:11:38 +0200

    x

--- app/src/main/java/com/demo/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/demo/jetpackcomposebasic/MainActivity.kt
@@ -3,18 +3,9 @@ package com.demo.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.dp
 import com.demo.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
