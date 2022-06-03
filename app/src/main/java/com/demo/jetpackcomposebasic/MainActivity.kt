60ea9f073c767f3cd5619a1a93b886c434b1bec5
Jorge Agullo <agullojorge@gmail.com>
2022-06-03 11:51:44 +0200

    Finish

--- app/src/main/java/com/demo/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/demo/jetpackcomposebasic/MainActivity.kt
@@ -3,18 +3,40 @@ package com.demo.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.demo.jetpackcomposebasic.navigation.Destination
 import com.demo.jetpackcomposebasic.navigation.NavigationHost
+import com.demo.jetpackcomposebasic.presentation.components.BottomNavigationBar
 import com.demo.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavigationHost()
+            MainScreen()
         }
+    }
+}
+
+@Composable
+fun MainScreen() {
+
+    val navController = rememberNavController()
+
+    val navigationItems = listOf(
+        Destination.Screen1,
+        Destination.Screen2,
+        Destination.Screen3
+    )
+
+    Scaffold(bottomBar = {
+        BottomNavigationBar(navController = navController, items = navigationItems)
+    }) {
+        NavigationHost(navController = navController)
     }
 }
 
@@ -22,6 +44,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicTheme {
-        NavigationHost()
+        MainScreen()
     }
 }
